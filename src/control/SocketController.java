ee9a32a40d8fb53f94f14137f2423ced141c9107
brady994 <eliana_palermiti@libero.it>
2016-11-04 17:37:30 +0100

    Changed the comunication of data with websocket, removed servlet 'HomeController' and added 'SocketController'

---
+++ src/control/SocketController.java
@@ -0,0 +1,79 @@
+package control;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+import model.MyCallback;
+import model.Option;
+import model.Result;
+import service.SolverDLV;
+
+@ServerEndpoint("/home")
+public class SocketController {
+
+	@OnOpen
+	public void onOpen(Session session) {
+		// Metodo eseguito all'apertura della connessione
+
+	}
+
+	@OnMessage
+	public String onMessage(String message, Session session) { 	// Metodo eseguito alla ricezione di un messaggio, la stringa 'message' rappresenta il messaggio
+		JsonParser parser = new JsonParser();
+		JsonElement element = parser.parse(message);
+		JsonObject object = element.getAsJsonObject();
+		String model = "";
+		Gson gson = new Gson();
+		String engine = gson.fromJson(object.get("engine"), String.class);
+		String program = gson.fromJson(object.get("program"), String.class);
+		Type optionType = new TypeToken<ArrayList<Option>>() {
+		}.getType();
+		ArrayList<Option> options = gson.fromJson(object.get("option"), optionType);
+		Result result = new Result();
+		switch (engine) {
+		case "dlv":
+			SolverDLV service = new SolverDLV(program);
+			MyCallback callback = new MyCallback(result);
+			if (service.checkOptionsDLV(options)) {
+				service.solveAsync(options, callback);
+			} else {
+				result.setError("Sorry, these options aren't valid");
+				model = result.toJson();
+			}
+			break;
+
+		default:
+			break;
+		}
+
+		while (result.getModel().equals("")) {
+			model = result.toJson();
+		}
+		model = result.toJson();
+		return model;
+
+	}
+
+	@OnClose
+	public void onClose(Session session) {
+		// Metodo eseguito alla chiusura della connessione
+	}
+
+	@OnError
+	public void onError(Throwable exception, Session session) {
+		// Metodo eseguito in caso di errore
+	}
+}
--- src/model/MyCallback.java
+++ src/model/MyCallback.java
@@ -1,38 +1,27 @@
 package model;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-
 import it.unical.mat.embasp.asp.AnswerSets;
 import it.unical.mat.embasp.base.Callback;
 import it.unical.mat.embasp.base.Output;
 
 public class MyCallback implements Callback {
 
-	public Result result;
-	HttpServletResponse response;
-	
-	public MyCallback(Result result,HttpServletResponse response ) {
-		this.result=result;
-		this.response=response;
+	private Result result;
+
+	public MyCallback(Result result) {
+		this.result = result;
 	}
-	
+
 	@Override
 	public void callback(Output output) {
-		AnswerSets answerSets=(AnswerSets)output;
-		if (answerSets.getAnswersets().isEmpty()){
+		AnswerSets answerSets = (AnswerSets) output;
+		if (answerSets.getAnswersets().isEmpty()) {
 			result.setModel("Sorry, there aren't answer sets for this program");
-		}else{
+		} else {
 			result.setModel(answerSets.getAnswerSetsString());
-			
+
 		}
-		try {
-			response.getWriter().write(result.toJson());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+
 	}
 
 	public Result getResult() {
